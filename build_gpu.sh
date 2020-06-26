ae00a250b3f39b66f37389ddc33ef390400fad83
Julien Pierret <dafrenchyman@gmail.com>
2020-06-25 23:01:19 -0700

    Updated pycharm version. Added in a gpu option

---
+++ build_gpu.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Helper functino to check a command exists
+command_exists () {
+    type "$1" &> /dev/null ;
+}
+
+# install x11 docker if not already installed
+if ! command_exists x11docker ; then
+    echo "Installing x11docker"
+    curl -fsSL https://raw.githubusercontent.com/mviereck/x11docker/master/x11docker | sudo bash -s -- --update
+fi
+
+docker build -t x11desktop_gpu -f Dockerfile_gpu .
---
+++ start_gpu.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+MEMORY=${1:-8g}
+DESKTOP_NAME=${2:-x11desktop}
+HOME_FOLDER=~/docker_volumes/${DESKTOP_NAME}/home
+
+# sudo password will be "x11docker"
+
+x11docker \
+    --desktop \
+    --hostnet \
+    --pulseaudio \
+    --sudouser \
+    --init=systemd \
+    --share /var/run/docker.sock \
+    --home="${HOME_FOLDER}" \
+    -- \
+    --security-opt \
+    seccomp=unconfined \
+    -- \
+    --shm-size="${MEMORY}" \
+    "x11desktop_gpu"
